33308cdcc983077a06783a7f9591b1230af1f6c1
Neidz <adamo7255@gmail.com>
2024-12-02 20:34:09 +0100

    day 1 part 2

--- hystorian-hysteria-day-1/src/main.rs
+++ hystorian-hysteria-day-1/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     io::{self, BufRead},
     usize,
 };
@@ -10,36 +11,55 @@ fn main() {
         .map(|line| {
             let line = line.unwrap();
 
-            let (first, second) = line.split_once(" ").unwrap();
-            let first_num: usize = first.trim().parse().unwrap();
-            let second_num: usize = second.trim().parse().unwrap();
+            let (left, right) = line.split_once(" ").unwrap();
+            let left_num: usize = left.trim().parse().unwrap();
+            let right_num: usize = right.trim().parse().unwrap();
 
-            (first_num, second_num)
+            (left_num, right_num)
         })
         .collect();
 
-    let (mut first_list, mut second_list): (Vec<usize>, Vec<usize>) =
+    let (mut left_list, mut right_list): (Vec<usize>, Vec<usize>) =
         num_pairs.iter().cloned().unzip();
 
-    first_list.sort();
-    second_list.sort();
+    left_list.sort();
+    right_list.sort();
 
-    assert!(first_list.len() == second_list.len());
+    assert!(left_list.len() == right_list.len());
 
-    let mut diff = vec![0; first_list.len()];
+    let mut diff = vec![0; left_list.len()];
 
-    for i in 0..first_list.len() {
-        let first_id = first_list[i];
-        let second_id = second_list[i];
+    for i in 0..left_list.len() {
+        let left_id = left_list[i];
+        let right_id = right_list[i];
 
-        if first_id >= second_id {
-            diff[i] = first_id - second_id
+        if left_id >= right_id {
+            diff[i] = left_id - right_id
         } else {
-            diff[i] = second_id - first_id
+            diff[i] = right_id - left_id
         }
     }
 
     let sum: usize = diff.iter().sum();
-
     println!("Sum of distances is: {}", sum);
+
+    let mut appearances = HashMap::new();
+
+    for id in right_list.iter() {
+        *appearances.entry(id).or_insert(0) += 1;
+    }
+
+    let mut similarities = vec![0; left_list.len()];
+
+    for i in 0..left_list.len() {
+        let left_id = left_list[i];
+
+        let right_appearances = appearances.get(&left_id).unwrap_or(&0);
+
+        let similarity_score = left_id * right_appearances;
+        similarities[i] = similarity_score;
+    }
+
+    let similarities_sum: usize = similarities.iter().sum();
+    println!("Sum of similarity scores is: {}", similarities_sum);
 }
