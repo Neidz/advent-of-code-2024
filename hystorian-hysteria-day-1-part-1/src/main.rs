9dcae0a8033324371638766aed4092870d16bcb0
Neidz <adamo7255@gmail.com>
2024-12-02 20:39:25 +0100

    split part 1 and 2 for first day

--- hystorian-hysteria-day-1-part-1/src/main.rs
+++ hystorian-hysteria-day-1-part-1/src/main.rs
@@ -1,5 +1,4 @@
 use std::{
-    collections::HashMap,
     io::{self, BufRead},
     usize,
 };
@@ -42,24 +41,4 @@ fn main() {
 
     let sum: usize = diff.iter().sum();
     println!("Sum of distances is: {}", sum);
-
-    let mut appearances = HashMap::new();
-
-    for id in right_list.iter() {
-        *appearances.entry(id).or_insert(0) += 1;
-    }
-
-    let mut similarities = vec![0; left_list.len()];
-
-    for i in 0..left_list.len() {
-        let left_id = left_list[i];
-
-        let right_appearances = appearances.get(&left_id).unwrap_or(&0);
-
-        let similarity_score = left_id * right_appearances;
-        similarities[i] = similarity_score;
-    }
-
-    let similarities_sum: usize = similarities.iter().sum();
-    println!("Sum of similarity scores is: {}", similarities_sum);
 }
